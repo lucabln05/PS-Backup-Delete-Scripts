404513d13388a19656c58919a602a1b13d8f4b4e
lucabln05 <77514119+lucabln05@users.noreply.github.com>
2022-11-09 11:51:32 +0100

    Add files via upload

---
+++ new_delete_in_logfile.ps1
@@ -0,0 +1,14 @@
+
+# Dieses Script splitet log.txt wenn "ROBOCOPY     ::     Robust File Copy for Windows"
+#und löscht alle abschnitte auser dem letzten
+
+
+$logfile = "log.txt"    # Pfad zum Logfile
+
+$logfile_content = Get-Content $logfile   # Inhalt des Logfiles in Variable speichern
+
+# Splitet das Logfile in Abschnitte wenn "ROBOCOPY" gefunden wird
+$logfile_content | Select-String -Pattern "ROBOCOPY" | Select-Object -last 1 | ForEach-Object {
+    $logfile_content | Select-Object -Skip ($_.LineNumber - 1) | Set-Content $logfile
+}
+
