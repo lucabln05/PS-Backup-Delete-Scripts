758cda4d987c528fd48f2b900680f7490c4ba6f0
lucabln05 <77514119+lucabln05@users.noreply.github.com>
2022-11-09 11:51:57 +0100

    Delete delete_in_logfile.ps1

--- delete_in_logfile.ps1
+++
@@ -1,9 +0,0 @@
-# ein log abschnitt ist 17.436 spalten lang
-# script löscht alle zeilen auser die letzten $LogLength zeilen
-
-$Logdeletedays = 1                      # lösche alle logdateien älter als $Logdeletedays 1 = ein tag
-$LogLength = -17437 * $Logdeletedays    # rechnet die länge des nicht zu löschenden logabschnitts aus
-
-$lines = Get-Content -Path log.txt      # liest die logdatei ein
-$lines = $lines[$LogLength..-1]         # löscht alle zeilen auser die letzten $LogLength zeilen
-$lines | Out-File -FilePath log.txt -Encoding UTF8  # schreibt die logdatei neu
