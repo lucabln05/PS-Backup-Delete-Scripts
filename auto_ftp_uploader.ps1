a127346dd99337d2ebd523f267d1b4ce6fa71bc9
lucabln05 <77514119+lucabln05@users.noreply.github.com>
2022-11-10 10:37:07 +0100

    Update auto_ftp_uploader.ps1
    Added Friday

--- auto_ftp_uploader.ps1
+++ auto_ftp_uploader.ps1
@@ -1,34 +1,26 @@
 # dieses Script lädt dateinen die älter als 48 stunden sind auf einen ftp server (ausgenommen wochenende)
-# docs https://renenyffenegger.ch/notes/Microsoft/dot-net/namespaces-classes/System/Net/WebClient/UploadFile; https://stackoverflow.com/questions/17829785/delete-files-older-than-15-days-using-powershell
+# docs https://renenyffenegger.ch/notes/Microsoft/dot-net/namespaces-classes/System/Net/WebClient/UploadFile;
 # test ftp server : https://dlptest.com/ftp-test/
-
 
 $ftpHost    = 'ftp.dlptest.com'             # ftp server
 $username   = 'dlpuser'                     # ftp user
 $password   = 'rNrKYTX9g7z3RgJRmxWuGHbeu'   # ftp password
 
-# alle dateien die älter als 48 stunden sind werden ausgelsen
-$files = Get-ChildItem -Path . -Filter *.zip -Recurse | Where-Object { $_.LastWriteTime -lt(Get-Date).AddHours(-48) -and $_.LastWriteTime.DayOfWeek -ne 'Saturday' -and $_.LastWriteTime.DayOfWeek -ne 'Sunday' }
+# alle dateien die älter als 48 stunden sind werden ausgelesen (im pfad wo das script liegt)
+$files = Get-ChildItem -Path . -Filter *.zip -Recurse | Where-Object { $_.LastWriteTime -lt(Get-Date).AddHours(-8)}
 
 
-#lädt die dateien auf den ftp server
-$files | ForEach-Object { $file = $_.Name;
-    $localFile = get-item $file;
-    $remoteFile = "ftp://${ftpHost}/${file}";
-    $uri = new-object System.Uri($remoteFile);
-    $webclient = new-object System.Net.WebClient;
-    $webClient.Credentials = new-object System.Net.NetworkCredential($username, $password);
-    $webclient.UploadFile($uri, $localFile.fullName)
-     }
-
-
-
-
-
-
-
-
-
-
-
-
+# wenn heute freitag, samstag oder sonntag ist, dann wird das script beendet
+if ((Get-Date).DayOfWeek -eq 'Friday' -or (Get-Date).DayOfWeek -eq 'Saturday' -or (Get-Date).DayOfWeek -eq 'Sunday' ){
+    exit
+} else {
+    #lädt die dateien auf den ftp server
+    $files | ForEach-Object { $file = $_.Name;
+        $localFile = get-item $file;
+        $remoteFile = "ftp://${ftpHost}/${file}";
+        $uri = new-object System.Uri($remoteFile);
+        $webclient = new-object System.Net.WebClient;
+        $webClient.Credentials = new-object System.Net.NetworkCredential($username, $password);
+        $webclient.UploadFile($uri, $localFile.fullName)
+         }
+}
