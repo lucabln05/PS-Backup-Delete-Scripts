393656a6efd4b6ffa79fc483f2ebe97c5cb4997b
lucabln05 <77514119+lucabln05@users.noreply.github.com>
2022-11-13 13:26:57 +0100

    Added new error messages

--- PYTHON-REBUILD/ftp-upload.py
+++ PYTHON-REBUILD/ftp-upload.py
@@ -1,3 +1,4 @@
+# https://www.geeksforgeeks.org/how-to-download-and-upload-files-in-ftp-server-using-python/
 # Importet Module
 import ftplib
 import os 
@@ -6,7 +7,7 @@ import datetime
 
 # Variablen
 today = datetime.date.today()
-path = 'test'
+path = 'ftp/test'
 delete_days = 2
 
 # FTP Verbindung
@@ -23,30 +24,50 @@ ftp_server.encoding = "utf-8"
 
 for f in os.listdir(path):      # listet alle Dateien im Verzeichnis
     try:
-        filedata, filetype = f.split('.')       # trennt Dateiendung von Dateinamen
-        number, filedate, filetime, clientname = filedata.split('-')        # trennt Dateinamen in Teile
-        file_year = int(filedate[:4])       # liest Jahr aus Dateinamen
-        file_month = int(filedate[4:6])     # liest Monat aus Dateinamen
-        file_day = int(filedate[6:8])       # liest Tag aus Dateinamen
+
+        try:
+            filedata, filetype = f.split('.')       # trennt Dateiendung von Dateinamen
+            number, filedate, filetime, clientname = filedata.split('-')        # trennt Dateinamen in Teile
+            file_year = int(filedate[:4])       # liest Jahr aus Dateinamen
+            file_month = int(filedate[4:6])     # liest Monat aus Dateinamen
+            file_day = int(filedate[6:8])       # liest Tag aus Dateinamen
+
+        except Exception as err:
+            print(f'''
+
+Fehler beim Lesen des Dateinamens: {f} 
+Bitte Dateinamen überprüfen auf Standard: Auftragsnummer-Datum-Uhrzeit-Benutzer.zip
+Fehlermeldung: {err}
+                    ''')
+        today_year = int(time.strftime("%Y"))       # liest aktuelles Jahr
+        today_month = int(time.strftime("%m"))      # liest aktuelles Monat
+        today_day = int(time.strftime("%d"))        # liest aktuelles Tag
+
+        diff_year = today_year - file_year          # berechnet Differenz zwischen aktuellem Jahr und Jahr aus Dateinamen
+        diff_month = today_month - file_month       # berechnet Differenz zwischen aktuellem Monat und Monat aus Dateinamen
+        diff_day = today_day - file_day             # berechnet Differenz zwischen aktuellem Tag und Tag aus Dateinamen
+
+        diff_days = diff_year * 360 + diff_month * 30 + today_day - file_day        # berechnet Differenz zwischen aktuellem Datum und Datum aus Dateinamen
+
+        if today.weekday() == 4 or today.weekday() == 5 or today.weekday() == 6 :       # prüft ob heute Freitag, Samstag oder Sonntag ist
+            print('Kein Upload am Wochenende')
+
+        elif diff_days > delete_days:       # prüft ob Datei älter als delete_days ist
+            try:
+                filedata = open(path + '/' + f, 'r+')       # öffnet Datei zum lesen und schreiben
+                print('Upload: ' + f)       # gibt Dateinamen aus
+                ftp_server.storbinary(f"STOR {f}", filedata)        # lädt Datei auf ftp server hoch
+                ftp_server.quit()           # trennt ftp Verbindung
+            except Exception as err:
+                print(f'''
+Fehler beim Upload der Datei: {f}
+Bitte überprüfen ob die Datei vorhanden ist und ob die FTP Verbindung funktioniert
+Fehlermeldung: {err}
+                        ''')
+
 
     except Exception as err:
-        print(err)
-    today_year = int(time.strftime("%Y"))       # liest aktuelles Jahr
-    today_month = int(time.strftime("%m"))      # liest aktuelles Monat
-    today_day = int(time.strftime("%d"))        # liest aktuelles Tag
-
-    diff_year = today_year - file_year          # berechnet Differenz zwischen aktuellem Jahr und Jahr aus Dateinamen
-    diff_month = today_month - file_month       # berechnet Differenz zwischen aktuellem Monat und Monat aus Dateinamen
-    diff_day = today_day - file_day             # berechnet Differenz zwischen aktuellem Tag und Tag aus Dateinamen
-
-    diff_days = diff_year * 360 + diff_month * 30 + today_day - file_day        # berechnet Differenz zwischen aktuellem Datum und Datum aus Dateinamen
-
-    if today.weekday() == 4 or today.weekday() == 5 or today.weekday() == 6 :       # prüft ob heute Freitag, Samstag oder Sonntag ist
-        print('Kein Upload am Wochenende')
-
-    elif diff_days > delete_days:       # prüft ob Datei älter als delete_days ist
-        filedata = open(path + '/' + f, 'r+')       # öffnet Datei zum lesen und schreiben
-        print('Upload: ' + f)       # gibt Dateinamen aus
-        ftp_server.storbinary(f"STOR {f}", filedata)        # lädt Datei auf ftp server hoch
-        ftp_server.quit()           # trennt ftp Verbindung
-
+        print(f'''
+Fehler ist aufgetreten: {err}
+Bitte Dateinamen überprüfen auf Standard: Auftragsnummer-Datum-Uhrzeit-Benutzer.zip
+                ''')
